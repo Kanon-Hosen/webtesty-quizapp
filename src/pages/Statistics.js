983cd5333078198e8de515ee5f3289bf013036e4
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-10-11 20:30:05 +0600

    responsive

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -4,16 +4,15 @@ import { contextData } from '../App';
 const Statistics = () => {
     const quizData = useContext(contextData)
     return (
-    <div className='mx-auto w-1/2 mt-14'>
-    <LineChart width={600} height={300} data={quizData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <div className='md:mx-auto w-full md:w-1/2 mt-14 overflow-hidden'>
+    <LineChart  width={500} height={300} data={quizData} margin={{ top: 5, right: 35, bottom: 5, left: 0 }}>
     <Line type="monotone" dataKey="total" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="name" />
     <YAxis />
     <Tooltip />
   </LineChart>
-
-        </div>
+</div>
     );
 };
 
